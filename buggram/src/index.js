9969246f8528b5e0c79127dd8ed8049e2d51ffb1
Luis Rodriguez <luis_rdz5@hotmail.com>
2017-04-17 16:29:32 -0500

    se agrego la configuracion para firebase

--- buggram/src/index.js
+++ buggram/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyA1zgI71xV_VwYSNijI2_0-h_fpI4nbNv0",
+    authDomain: "buggram-aecee.firebaseapp.com",
+    databaseURL: "https://buggram-aecee.firebaseio.com",
+    projectId: "buggram-aecee",
+    storageBucket: "buggram-aecee.appspot.com",
+    messagingSenderId: "593230134973"
+});
+
+  
+
 
 ReactDOM.render(
   <App />,
